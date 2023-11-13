cae5ff6e6d1a78e20a9a6a8f5530979162a1fcf9
lesavourey <lesavourey@lesavoureys-MacBook-Air.local>
2023-11-13 18:09:58 +0100

    Generation of extension over maximal totally real subfield cyclo.

--- src/generation.c
+++ src/generation.c
@@ -3,7 +3,7 @@
 /* POL. DEFINING FIELDS */
 
 /* generates irreducible pol. with at least one real root */
-Pol_field_creation_real(field_dim, size_coeff, {var=y})= {
+Pol_field_creation_real(field_dim, size_coeff, {var=y}) = {
   my(p, r1, R);
   r1 = 0;
   while(r1==0,
@@ -23,64 +23,61 @@ Pol_field_creation_real(field_dim, size_coeff, {var=y})= {
 
 
 /* generates irreducible pol. with at least one complex root */
-Pol_field_creation_CC(field_dim, size_coeff, {var=y})=
-  {
-    my(p, r1, R);
-    r1 = field_dim;
-    while(r1==field_dim,
-	  p = vector(field_dim, i, random(2*2^size_coeff)-2^size_coeff);
-	  p = Pol(p, var);
-	  p += var^field_dim;
-	  while(!polisirreducible(p),
-		p = vector(field_dim, i, random(2*2^size_coeff)-2^size_coeff);
-		p = Pol(p, var);
-		p += var^field_dim;
-		);
-	  R = polrootsreal(p);
-	  r1 = length(R);
-	  );
-    return(p);
-  };
-
-
-/* generates irreducible pol. with no real root */
-Pol_field_creation_complex(field_dim, size_coeff, {var=y})=
-  {
-    my(p, r1, R);
-    if (Mod(field_dim,2)!=0,
-	print("ERROR WRONG DIM");
-	return 0; ,
-       
-	r1 = 1;
-	while(r1!=0,
+Pol_field_creation_CC(field_dim, size_coeff, {var=y}) = {
+  my(p, r1, R);
+  r1 = field_dim;
+  while(r1==field_dim,
+	p = vector(field_dim, i, random(2*2^size_coeff)-2^size_coeff);
+	p = Pol(p, var);
+	p += var^field_dim;
+	while(!polisirreducible(p),
 	      p = vector(field_dim, i, random(2*2^size_coeff)-2^size_coeff);
 	      p = Pol(p, var);
 	      p += var^field_dim;
-	      while(!polisirreducible(p),
-		    p = vector(field_dim, i, random(2*2^size_coeff)-2^size_coeff);
-		    p = Pol(p, var);
-		    p += var^field_dim;
-		    );
-	      R = polrootsreal(p);
-	      r1 = length(R);
 	      );
+	R = polrootsreal(p);
+	r1 = length(R);
 	);
-    return(p);
-  };
+  return(p);
+};
+
+
+/* generates irreducible pol. with no real root */
+Pol_field_creation_complex(field_dim, size_coeff, {var=y}) = {
+  my(p, r1, R);
+  if (Mod(field_dim,2)!=0,
+      print("ERROR WRONG DIM");
+      return 0; ,
+       
+      r1 = 1;
+      while(r1!=0,
+	    p = vector(field_dim, i, random(2*2^size_coeff)-2^size_coeff);
+	    p = Pol(p, var);
+	    p += var^field_dim;
+	    while(!polisirreducible(p),
+		  p = vector(field_dim, i, random(2*2^size_coeff)-2^size_coeff);
+		  p = Pol(p, var);
+		  p += var^field_dim;
+		  );
+	    R = polrootsreal(p);
+	    r1 = length(R);
+	    );
+      );
+  return(p);
+};
 
 
 /* generates irreducible polynomial */
-Pol_field_creation(field_dim, size_coeff, {var=y}) =
-  {
-    my(p);
-    p = sum(k=0, field_dim-1, (random(2*2^size_coeff)-2^size_coeff)*var^k);
-    p += var^field_dim;
-    while(!polisirreducible(p),
-	  p = sum(k=0, field_dim-1, (random(2*2^size_coeff)-2^size_coeff)*var^k);
-	  p += var^field_dim;
-	  );
-    return(p);
-  };
+Pol_field_creation(field_dim, size_coeff, {var=y}) = {
+  my(p);
+  p = sum(k=0, field_dim-1, (random(2*2^size_coeff)-2^size_coeff)*var^k);
+  p += var^field_dim;
+  while(!polisirreducible(p),
+	p = sum(k=0, field_dim-1, (random(2*2^size_coeff)-2^size_coeff)*var^k);
+	p += var^field_dim;
+	);
+  return(p);
+};
 
 
 /* POL. DEFINING EQUATIONS */
@@ -176,33 +173,31 @@ Equation_creation_nf_multiquad(pol_field, degree_eq, size_roots) = {
    ext_dim = degree of new pol
    size_coeff = size_coeff of new pol */
 
-Pol_ext_creation(pol_ground, ext_dim, size_coeff, {varg=z, vare=y}) =
-  {
-    my(q, pg);
+Pol_ext_creation(pol_ground, ext_dim, size_coeff, {varg=z, vare=y}) = {
+  my(q, pg);
 
-    pg = subst(pol_ground, variable(pol_ground), varg);
+  pg = subst(pol_ground, variable(pol_ground), varg);
    
-    q = Mod(Pol_eq_creation_field(ext_dim-1, size_coeff, pg, vare, varg), pg);
+  q = Mod(Pol_eq_creation_field(ext_dim-1, size_coeff, pg, vare, varg), pg);
    
-    q = q + vare^ext_dim;
+  q = q + vare^ext_dim;
    
-    while(!polisirreducible(q),
+  while(!polisirreducible(q),
 
-	  q = Mod(Pol_eq_creation_field(ext_dim-1, size_coeff, pg, vare, varg), pg);
+	q = Mod(Pol_eq_creation_field(ext_dim-1, size_coeff, pg, vare, varg), pg);
    
-	  q = q + vare^ext_dim;
+	q = q + vare^ext_dim;
    	 
-	  );
+	);
    
-    return(q);
-  };
+  return(q);
+};
 
 
 /* create rel ext. fom scratch */
 /* dim = vector of dim
    size_coeff = vec of size_coeff  */
-Pol_rel_ext_creation(dim, size_coeff, {varg=z, vare=y}) =
-  {
+Pol_rel_ext_creation(dim, size_coeff, {varg=z, vare=y}) = {
     my(p, q);
    
     p = Pol_field_creation(dim[1], size_coeff[1], varg);
@@ -213,6 +208,7 @@ Pol_rel_ext_creation(dim, size_coeff, {varg=z, vare=y}) =
   };
 
 
+/* /!\ to modify */
 /* create cyclo rel ext. fom scratch */
 Cyclo_rel_ext_creation(prime, exp_g, exp_e, {varg=z, vare=y}) = {
   my(p, q, pabs);
@@ -223,6 +219,74 @@ Cyclo_rel_ext_creation(prime, exp_g, exp_e, {varg=z, vare=y}) = {
   
   return([p, q, pabs]);
 };
+
+/* create extension of cyclotomic field L over totally real subfield k
+ L is given by the conductor "cond" and k is the maximal totally real subfield
+ of the cyclotomic field given by K = Q(zeta_{cond / prime }) */
+Cyclo_real_rel_ext_creation(cond, prime, exp, {varg=z, vare=y, varK=t, varL=T}) = {
+  my(p, q, pabs, qabs, zmL, zmK, eta, mK, mLK, b, i, r1, r2);
+
+  mK = cond / prime^exp; mLK = cond / mK;  /* note that cond = mL */
+  qabs = polcyclo(cond, varL);	/* pol of L */
+  pabs = polcyclo(mK, varK);	/* pol of K */
+  p = concat([], polsubcyclo(mK, eulerphi(mK)/2, varg)); /* possible pols of K^+ */
+  /* find one possible pol for K^+ : need totally real one */
+  b = 0;
+  i = 0;
+  while (!b,
+	 i += 1;
+	 [r1, r2] = MySignature(p[i]);
+	 b = (r2==0);
+	 );
+  p = p[i];
+
+  zmL = Mod(varL, qabs);
+  
+  /* first find minimal pol. of zeta_cond expressed as pol. w. coeff in L */
+  /* set of Gal(L/K) = {i | i==1[mK] && gcd(i, mLK)==1}  */
+  GLK = [];
+  print(mLK);
+  for (i = 1,  mLK,
+	 if (gcd(i, mLK)==1,
+	     GLK = concat(GLK, lift(chinese(Mod(1, mK), Mod(i, mLK))));
+	     );
+       );
+
+  /* minimal pol. over K^+ will be prod_{s in GLK} (X - zeta_cond^s) * (X - zeta_cond^(-s) ) */
+  mu = 1;
+  for (i=1, #GLK,
+	 mu *= ((vare - zmL^GLK[i]) * (vare - zmL^(-GLK[i])));
+       );
+
+  /* generator of totally real real subfield K^+ */
+  zmK = zmL^mLK;
+  eta = zmK + zmK^(-1);
+
+  
+  /* now create matrix of basis of K^+ expressed in K */
+  B = vector(eulerphi(mK)/2, i, eta^(i-1));
+  M = Vecrev(lift(B[1]), poldegree(qabs));
+  for (i = 2, #B,
+	 M = matconcat([M ; Vecrev(lift(B[i]), poldegree(qabs))])
+       );
+
+  
+  
+  return([p, mu, qabs, M]);
+  
+  /* zmK = Mod(varK, pabs); */
+  
+  /* q = vare^(2*prime)-(zmK); */
+  
+  /* pabs = polcyclo(cond); */
+  /* p = polcyclo(cond/prime^exp); */
+  /* p = polcyclo(cond, varg); */
+  /* q = vare^(prime^exp_e)-varg; */
+  /* pabs = polcyclo(prime^(exp_g + exp_e)); */
+  
+  /* return([p, q, pabs]); */
+};
+
 
 /* create cyclo rel ext. fom scratch */
 Cyclo_rel_ext_creation_2(cond, prime, exp, {varg=z, vare=y}) = {
@@ -239,48 +303,45 @@ Cyclo_rel_ext_creation_2(cond, prime, exp, {varg=z, vare=y}) = {
 
 /* ELEMENTS AND POL CREATION */
 
-/* create g in Ke/Kg ; output is a multivar. pol. */
-/* one can retrieve g in vector form by applying Multipol_to_vec */
-Rel_element_creation(dim_vec, s_coeff, {varg=z, vare=y})=
-  {
-    my(a,w);
-    w = vecsum(powers(varg, dim_vec[1]-1));
-    a = vector(dim_vec[2], i,
-	       random((2*2^s_coeff)*varg^(dim_vec[1]-1))-2^s_coeff*w);
-    return(Pol(a, vare));
-  };
+  /* create g in Ke/Kg ; output is a multivar. pol. */
+  /* one can retrieve g in vector form by applying Multipol_to_vec */
+Rel_element_creation(dim_vec, s_coeff, {varg=z, vare=y}) = {
+  my(a,w);
+  w = vecsum(powers(varg, dim_vec[1]-1));
+  a = vector(dim_vec[2], i,
+	     random((2*2^s_coeff)*varg^(dim_vec[1]-1))-2^s_coeff*w);
+  return(Pol(a, vare));
+};
 
 
 /* create g in Ke/Kg ; output is a multivar. pol. */
 /* one can retrieve g in vector form by applying Multipol_to_vec */
-Rel_element_creation_mod(pol_vec, s_coeff, {varg=z, vare=y})=
-  {
-    my(a,w,dg, de);
-    dg = poldegree(pol_vec[1]);
-    de = poldegree(pol_vec[2]);
+Rel_element_creation_mod(pol_vec, s_coeff, {varg=z, vare=y}) = {
+  my(a,w,dg, de);
+  dg = poldegree(pol_vec[1]);
+  de = poldegree(pol_vec[2]);
    
-    w = vecsum(powers(varg, dg-1));
-    a = vector(de, i,
-	       Mod(random((2*2^s_coeff)*varg^(dg-1))-2^s_coeff*w, pol_vec[1]));
-    return(Mod(Pol(a, vare), pol_vec[2]));
-  };
+  w = vecsum(powers(varg, dg-1));
+  a = vector(de, i,
+	     Mod(random((2*2^s_coeff)*varg^(dg-1))-2^s_coeff*w, pol_vec[1]));
+  return(Mod(Pol(a, vare), pol_vec[2]));
+};
 
 
 /* create Pol in Ke/Kg[x] ; output is a multivar. pol. */
 /* one can retrieve Pol in vector form by applying Multipol_to_vec */
-Rel_pol_creation(dim_vec, deg, s_coeff, {varp=x, varg=z, vare=y})=
-  {
-    my(a);
-    /* w = vecsum(powers(varp, deg)); */
-    a = vector(deg+1, i,
-	       Rel_element_creation(dim_vec, s_coeff, varg, vare));
-    return(Pol(a, varp));
-  };
+Rel_pol_creation(dim_vec, deg, s_coeff, {varp=x, varg=z, vare=y}) = {
+  my(a);
+  /* w = vecsum(powers(varp, deg)); */
+  a = vector(deg+1, i,
+	     Rel_element_creation(dim_vec, s_coeff, varg, vare));
+  return(Pol(a, varp));
+};
 
 
 /* create Pol in Ke/Kg[x] ; output is a multivar. pol. */
 /* one can retrieve Pol in vector form by applying Multipol_to_vec */
-Rel_pol_creation_mod(pol_vec, deg, s_coeff, {varp=x, varg=z, vare=y})=  {
+Rel_pol_creation_mod(pol_vec, deg, s_coeff, {varp=x, varg=z, vare=y}) = {
   my(a);
   /* w = vecsum(powers(varp, deg)); */
   a = vector(deg+1, i,
@@ -291,7 +352,7 @@ Rel_pol_creation_mod(pol_vec, deg, s_coeff, {varp=x, varg=z, vare=y})=  {
 
 
 /* create equation of the form h = q(g) with q in ZZ[X] or Ke/Kg[X] */
-Rel_equation_creation(pol_vec, s_root, degree_eq, s_eq, type)= {
+Rel_equation_creation(pol_vec, s_root, degree_eq, s_eq, type) = {
   my(g, h, q, field_dim);
    
   field_dim = poldegree(pol_field);
@@ -317,7 +378,7 @@ Rel_equation_creation(pol_vec, s_root, degree_eq, s_eq, type)= {
 
 
 /* create equation in Ke/Kg[X] split over Ke/Kg */
-Rel_equation_creation_split(pol_vec, degree_eq, size_eq)= {
+Rel_equation_creation_split(pol_vec, degree_eq, size_eq) = {
   my(v, eq);
   if (degree_eq == 0,
       eq = 1; ,
@@ -330,7 +391,7 @@ Rel_equation_creation_split(pol_vec, degree_eq, size_eq)= {
 
 
 /* create equation in Ke/Kg[X] split over Ke/Kg */
-Rel_equation_creation_multiquad(pol_vec, degree_eq, size_eq)=   {
+Rel_equation_creation_multiquad(pol_vec, degree_eq, size_eq) = {
   my(v, eq, s, dg, de, t);
   dg = poldegree(pol_vec[1]);
   de = poldegree(pol_vec[2]);
